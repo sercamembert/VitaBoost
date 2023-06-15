ff04fd7b8a7678c1b2fc45f99c65a9da7a33cf85
Filip Maszota <filipmaszota3@gmail.com>
2023-06-15 12:00:15 +0200

    add newsletter section

--- src/components/newsletter/Newsletter.jsx
+++ src/components/newsletter/Newsletter.jsx
@@ -1,7 +1,7 @@
 const Newsletter = () => {
   return (
     <section className="bg-white dark:bg-gray-900">
-      <div className="mx-auto max-w-screen-xl px-4 py-8 pb-24 pt-24 lg:px-6 lg:py-16">
+      <div className="mx-auto max-w-screen-xl px-4 py-24 py-8 lg:px-6 lg:py-16">
         <div className="mx-auto max-w-screen-md sm:text-center">
           <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
             Sign up for our newsletter
