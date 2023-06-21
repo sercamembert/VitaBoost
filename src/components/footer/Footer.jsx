fb5c1881ad694032201e4ea7053fb2ec52bc30a3
Filip Maszota <filipmaszota3@gmail.com>
2023-06-21 13:23:06 +0200

    fix footer links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,51 +1,53 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <section className="bg-primary">
       <div className="mx-auto max-w-screen-xl space-y-8 overflow-hidden px-4 py-12 sm:px-6 lg:px-8">
         <nav className="-mx-5 -my-2 flex flex-wrap justify-center">
           <div className="px-5 py-2">
-            <a
-              href="#"
+            <Link
+              to="/#about"
               className="text-base leading-6 text-white hover:text-gray-900"
             >
               About
-            </a>
+            </Link>
           </div>
           <div className="px-5 py-2">
-            <a
-              href="#"
+            <Link
+              to="/#products"
               className="text-base leading-6 text-white hover:text-gray-900"
             >
               Products
-            </a>
+            </Link>
           </div>
           <div className="px-5 py-2">
-            <a
-              href="#"
+            <Link
+              to="/contact"
               className="text-base leading-6 text-white hover:text-gray-900"
             >
               Contact
-            </a>
+            </Link>
           </div>
           <div className="px-5 py-2">
-            <a
-              href="#"
+            <Link
+              to="/privacy"
               className="text-base leading-6 text-white hover:text-gray-900"
             >
               Privacy
-            </a>
+            </Link>
           </div>
           <div className="px-5 py-2">
-            <a
-              href="#"
+            <Link
+              to="/terms"
               className="text-base leading-6 text-white hover:text-gray-900"
             >
               Terms
-            </a>
+            </Link>
           </div>
         </nav>
         <div className="mt-8 flex justify-center space-x-6">
-          <a href="#" className="text-white hover:text-black">
+          <Link href="#" className="text-white hover:text-black">
             <span className="sr-only">Facebook</span>
             <svg
               className="h-6 w-6"
@@ -59,8 +61,8 @@ const Footer = () => {
                 clipRule="evenodd"
               ></path>
             </svg>
-          </a>
-          <a href="#" className="text-white hover:text-black">
+          </Link>
+          <Link href="#" className="text-white hover:text-black">
             <span className="sr-only">Instagram</span>
             <svg
               className="h-6 w-6"
@@ -74,8 +76,8 @@ const Footer = () => {
                 clipRule="evenodd"
               ></path>
             </svg>
-          </a>
-          <a href="#" className="text-white hover:text-black">
+          </Link>
+          <Link href="#" className="text-white hover:text-black">
             <span className="sr-only">Twitter</span>
             <svg
               className="h-6 w-6"
@@ -85,7 +87,7 @@ const Footer = () => {
             >
               <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
             </svg>
-          </a>
+          </Link>
         </div>
         <p className="mt-8 text-center text-base leading-6 text-white">
           © 2023 VitaBoost, Inc. All rights reserved.
